6f8e6195a1c01e93b2ecb8b2a15dbb8bbbb98d4f
tanvimandhan <tanvimandhan34@gmail.com>
2025-08-06 22:19:21 +0530

    commit

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use('/upload', express.static('upload'));
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
