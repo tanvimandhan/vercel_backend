c6cab2df939eaaad47247ced052008e8181300d1
tanvimandhan <tanvimandhan34@gmail.com>
2025-08-06 23:06:18 +0530

    fix cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://vercel-frontend-alpha-pearl.vercel.app/",
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
